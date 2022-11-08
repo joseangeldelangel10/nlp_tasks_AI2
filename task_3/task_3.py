636836865174fc872475e28a74eb4926aed619fe
joseangeldelangel10 <joseangeldelangel10@gmail.com>
2022-11-08 11:55:57 -0600

    file name typo corrected

--- task_3/task_3.py
+++ task_3/task_3.py
@@ -1,9 +1,8 @@
 f = open("es-en/europarl-v7.es-en.en", "r")
-
-max_iter = 10
+maximum_iter = 10
 i = 1
 for line in f:
     print(line)
-    if i == max_iter:
+    if i == maximum_iter:
         break
     i += 1
